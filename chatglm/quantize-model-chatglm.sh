52c5fd348353b187d20227baa4157406dc9b40c5
WayneCommand <wayne@waynecommand.com>
2024-10-11 11:30:31 +0800

    Update quantize-model-chatglm.sh

--- chatglm/quantize-model-chatglm.sh
+++ chatglm/quantize-model-chatglm.sh
@@ -2,7 +2,7 @@
 DEFAULT_WORK_PATH=/tmp/works
 RUNNING_PATH=${WORK_PATH-$DEFAULT_WORK_PATH}
 
-mkdir $RUNNING_PATH/models
+mkdir -p $RUNNING_PATH/models
 
 python3 $RUNNING_PATH/chatglm.cpp/chatglm_cpp/convert.py -i $RUNNING_PATH/GLM-4-9B-Chat -t q4_0 -o $RUNNING_PATH/models/chatglm-ggml.bin
 

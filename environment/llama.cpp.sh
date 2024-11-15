345cf22fb73c0f3ad2cc7a6bd10f11d9f84f39d4
WayneCommand <wayne@waynecommand.com>
2024-11-15 16:09:02 +0800

    Update llama.cpp.sh

--- environment/llama.cpp.sh
+++ environment/llama.cpp.sh
@@ -11,7 +11,7 @@ git clone https://github.com/ggerganov/llama.cpp.git
 
 ## install dependency
 cd llama.cpp
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
 sudo apt update && sudo apt install libopenblas-dev -y
 make GGML_OPENBLAS=1 -j 8
 
